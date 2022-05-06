0309858063c43592c28e5e9dc448759223162f83
Dang Viet Long <vietlong.dang@sailpoint.com>
2022-05-06 21:04:57 +0200

    finish with menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import "./index.css"
 import { StaticImage } from "gatsby-plugin-image"
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
-import Button from "../components/button/button"
 import News from "../components/lastnews/lastnews"
 import Works from "../components/works/works"
 import Specialities from "../components/specialities/specialities"
@@ -17,18 +16,62 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes"
 
 const IndexPage = () => {
   const menuItems = [
-    {displayName: 'Про кафедру', open: false, hide: false, subItems: ['Склад кафедри', 'Історія кафедри', 'Наші випускники']},
+    {
+      displayName: 'Про кафедру',
+      open: false,
+      hide: false,
+      subItems: [
+        {displayName: 'Склад кафедри', link: '/teacher/teacher'},
+        {displayName: 'Історія кафедри', link: '/history/history'},
+        {displayName: 'Наші випускники', link: '/graduates/graduates'},
+      ]
+    },
     {displayName: 'Учбовий процес', open: false, hide: false, subItems: []},
-    {displayName: 'Наука', open: false, hide: false, subItems: []},
-    {displayName: 'Абітурієнтам', open: false, hide: false, subItems: []},
-    {displayName: 'Студентам', open: false, hide: false, subItems: []},
-    {displayName: 'Партнери', open: false, hide: false, subItems: []},
-    {displayName: 'Контакты', open: false, hide: false, subItems: []}
+    {displayName: 'Наука', open: false, hide: false, subItems: [
+        {displayName: 'Лабараторія', link: '/teacher/teacher'},
+        {displayName: 'Наукові напрями', link: '/directions/directions'},
+        {displayName: 'Студентська наука', link: '/studentscience/studentscience'},
+        {displayName: 'Роботи студентів', link: '/works/works'},
+        {displayName: 'Гуртки кафедри', link: '/sections/sections'},
+        {displayName: 'Запропонувати тему', link: '/offer/offer'}
+      ]
+    },
+    {displayName: 'Абітурієнтам', open: false, hide: false, subItems: [
+        {displayName: 'Правила вступу', link: '/rules/rules'},
+        {displayName: 'Спеціальність', link: '/admissions/admissions'},
+        {displayName: 'Наша реклама', link: '/advertising/advertising'}
+      ]
+    },
+    {displayName: 'Студентам', open: false, hide: false, subItems: [
+        {displayName: 'Розклад', link: 'http://cist.kture.kharkov.ua'},
+        {displayName: 'Ресурси', link: '/resources/resources'},
+        {displayName: 'Студентське життя', link: '/student-life/student-life'},
+        {displayName: 'Анотації дисциплін', link: '/annotations/annotations'},
+        {displayName: 'Науково-дослід. робота', link: '/science-work/science-work'},
+        {displayName: 'Вибір студента', link: '/student-choise/student-choise'},
+        {displayName: 'Дипломування', link: '/diploma/diploma'},
+        {displayName: 'Обговорення ОП', link: '/discuss/discuss'}
+      ]
+    },
+    {displayName: 'Партнери', open: false, hide: false, link: '#' },
+    {displayName: 'Контакты', open: false, hide: false, link: '#'}
   ]
 
   const { t } = useTranslation()
   const [burgerMenu, setBurgerMenu] = useState(false)
   const [menuItemsState, setMenuItemsState] = useState(menuItems);
+
+  const toggleSubMenu = (key) => {
+    setMenuItemsState(menuItemsState.map((item, index) => {
+      if (index === key) {
+        item.open = !item.open
+      } else {
+        item.hide = !item.hide
+      }
+
+      return item
+    }))
+  }
 
   return (
     <div>
@@ -82,28 +125,34 @@ const IndexPage = () => {
                     menuItemsState[key].open ? 'list-item-sm' :
                     menuItemsState[key].hide ? 'list-item-sm_none' : 'list-item-sm'
                   }
-                  onClick={() =>
-                    setMenuItemsState(menuItemsState.map((item, index) => {
-                      if (index === key) {
-                        item.open = !item.open
-                      } else {
-                        item.hide = !item.hide
-                      }
-
-                      return item
-                    }))}
                   key={item.displayName + key.toString()}
                 >
 
                   {
                     menuItemsState[key].open ? (
                     <ul className='list-sub-menu-sm'>
-                      <li className="list-item-sm_active list-item-sm">{item.displayName}</li>
+                      <li className="list-item-sm_active list-item-sm">
+                        <span
+                          onClick={() => toggleSubMenu(key)}
+                          className="list-item_link"
+                        >
+                          {item.displayName}
+                        </span>
+                      </li>
                       {item.subItems.map((subItem, subItemIndex) => (
-                        <li className="list-item-sm" key={subItem + subItemIndex.toString()}>{subItem}</li>
+                        <li className="list-item-sm" key={subItem + subItemIndex.toString()}>
+                          <Link className="list-item_link" to={subItem.link}>{subItem.displayName}</Link>
+                        </li>
                       ))}
                     </ul>
-                  ) : item.displayName
+                  ) : item.link ?
+                    <Link className="list-item_link" to={item.link}>{item.displayName}</Link>
+                     :
+                    <span
+                      onClick={() => toggleSubMenu(key)}
+                      className="list-item_link">
+                      {item.displayName}
+                    </span>
                   }
                 </li>
               ))}
@@ -123,7 +172,6 @@ const IndexPage = () => {
               className="intro__image-sm"
             />
             <h1 className="intro__title">
-              {" "}
               Харківський національний університет радіоелектроніки
             </h1>
             <h2 className="intro__subtitle">
