90f6f4674204d4b5fcf4f8c8432118b6711da133
Dmytro Sukhomlyn <suhoidimka@gmail.com>
2022-05-15 17:32:32 +0300

    advertising

--- src/pages/advertising/index.js
+++ src/pages/advertising/index.js
@@ -6,12 +6,41 @@ import Footer from "src/components/footer"
 import News from "src/components/lastnews"
 import Title from "src/components/title"
 const AdvertisingPage = () => {
+  const advertisings = [
+    {
+      link: "https://www.youtube.com/embed/SO8ZLsSg-eA",
+    },
+    {
+      link: "https://www.youtube.com/embed/nYLdRFXBnDc",
+    },
+    {
+      link: "https://www.youtube.com/embed/emft9kIoRJQ",
+    },
+  ]
   return (
     <div>
       <Header />
       <Title>Наша реклама</Title>
       <div className="advertising">
-        <div className="container"></div>
+        <div className="container">
+          <div className="advertising__inner">
+            {advertisings.map((advertising, index) => (
+              <div className="advertising__item" key={index.toString()}>
+                <iframe
+                  width="630"
+                  height="352"
+                  src={advertising.link}
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  className="advertising__video"
+                  
+                ></iframe>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       <News />
       <Footer />
